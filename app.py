9fe2853e36a199a546f426731f5007183223fd20
Rakesh TN <tnrakesh68@outlook.com>
2024-12-20 04:48:19 +0530

    Updating

--- app.py
+++ app.py
@@ -1,5 +1,30 @@
 # Variables 
-name = "Rakesh is"
-print(name+"Batman")
-print(name,"Lucky")
-print(name,"in Bangalore")
+name = "Rakesh"
+print(name+"is Batman")
+print(name,"is Lucky")
+print(name,"is in Bangalore")
+
+#Strings 
+print('Hi, I\'m,'+name+'\nI Living in bangalore')
+print(name.upper())
+print(name.lower())
+print(name.isupper())
+print(name.islower())
+print(name.upper().isupper())
+print(len(name))
+print(name.replace('k','j'))
+
+# Number
+number = 100
+
+print(abs(-55))
+print(max(4,8,12,2))
+print(min(4,8,12,2))
+print(round(3.2))
+print(round(3.5))
+print(bin(8)) #binary value of number
+
+from math import *
+
+print (sqrt(100))
+
---
+++ input.py
@@ -0,0 +1,5 @@
+# Getting User Input 
+name =input("Enter Your Name: ")
+age = input("Enter Your age: ")
+
+print('Hi,',name,'we get to know that your age is',age)
