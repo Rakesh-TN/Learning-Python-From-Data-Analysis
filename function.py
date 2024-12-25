c4785b2baa6b618825949c7f6d029ff2c3e4cc8d
Rakesh TN <tnrakesh68@outlook.com>
2024-12-25 14:29:02 +0530

    Done

--- function.py
+++ function.py
@@ -1,9 +1,22 @@
+print("- Function")
 #def - define 
 
 def greetings_function(name, age):
     print("Hello", name ,"Your age is", age)
     
-name = input('Enter Your Name :')
-age =  input('Enter Your Age :')  
+# name = input('Enter Your Name :')
+# age =  input('Enter Your Age :')  
 
-greetings_function(name, age)
+greetings_function("John", 24)
+# greetings_function(name, age)
+
+print("- Function with return value")
+# Return Statement
+
+def addNumbers(num1, num2):
+    return num1 + num2
+
+num1 = int(input('Enter First Number :'))
+num2 = int(input('Enter Second Number :'))
+
+print(addNumbers(num1, num2))
